e1ae7d02708d4e7053d169c8d8d02aeab23bca07
Daniel Correa <yo@danielgara.com>
2022-09-20 10:20:50 -0500

    imp: minor symbol

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="copyright mt-4 text-center text-white">
       <div className="text-center p-4">
-        Copyright 2022 -{" "}
+        Copyright © 2022  -{" "}
         <a
           target="_blank"
           className="text-reset fw-bold text-decoration-none"
